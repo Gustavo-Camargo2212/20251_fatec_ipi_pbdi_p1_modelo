ae32843e1a9b6f06dd0ccdfb7e219d20e9a88419
Gustavo <gu-gustavo1999@hotmail.com>
2025-04-29 10:40:00 -0300

    feat(p1): encontra aprovados com pais PhDs

--- script.sql
+++ script.sql
@@ -16,6 +16,33 @@ CREATE TABLE ESTUDANTES (
 -- ----------------------------------------------------------------
 -- 2 Resultado em função da formação dos pais
 --escreva a sua solução aqui
+DO $$
+DECLARE
+    cur_estudantes CURSOR FOR
+        SELECT id, mother_edu, father_edu, grade FROM estudantes
+        WHERE grade = 1
+          AND (mother_edu = 1 OR father_edu = 1);
+
+    v_id INT;
+    v_mother_edu INT;
+    v_father_edu INT;
+    v_grade INT;
+
+    contador_aprovados INT := 0;
+BEGIN
+    OPEN cur_estudantes;
+
+    LOOP
+        FETCH cur_estudantes INTO v_id, v_mother_edu, v_father_edu, v_grade;
+        EXIT WHEN NOT FOUND;
+
+        contador_aprovados := contador_aprovados + 1;
+    END LOOP;
+
+    CLOSE cur_estudantes;
+
+    RAISE NOTICE 'Total de alunos aprovados com pelo menos um dos pais PhD: %', contador_aprovados;
+END $$;
 
 
 -- ----------------------------------------------------------------
